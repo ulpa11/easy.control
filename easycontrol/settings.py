bc3f14668b57bbedcaedcb41633fe982075fd135
ashutosh raghuwanshi <58286940+ulpa11@users.noreply.github.com>
2021-11-19 17:27:45 +0530

    Update settings.py

--- easycontrol/settings.py
+++ easycontrol/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-))i!&vga%cnis546t)qs@8o3iid2=8-6g00x_gevts4t_2c)k!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://easy---control.herokuapp.com/',"127.0.0.1:8000/",'127.0.0.1']
+ALLOWED_HOSTS = ['easy---control.herokuapp.com/',"127.0.0.1:8000/",'127.0.0.1']
 
 
 # Application definition
